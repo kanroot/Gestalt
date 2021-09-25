344d1a39574e6886be50d8bfe9ed24ff42e56a8d
kan <root.kan.ss@gmai.com>
2021-09-25 20:25:53 +0000

    El girado de los position2d funciona

--- Src/Laugh/TypeOfShoots/ShootEnemy.cs
+++ Src/Laugh/TypeOfShoots/ShootEnemy.cs
@@ -8,10 +8,14 @@ namespace Laugh.TypeOfShoots
 	{
 		private List<Node2D> spawnList;
 		private Node2D spawmNode;
+		private float angle;
+		private float sumAngle = 0;
 
 		public ShootEnemy(PackedScene spawn, int countSpawn, PackedScene bullet, float speedBullet,
 			KinematicBody2D entity) : base(spawn, countSpawn, bullet, speedBullet, entity)
 		{
+			angle = (float) 360 / CountSpawn;
+			sumAngle = angle;
 		}
 
 		public override void CreateSpawn()
@@ -20,21 +24,18 @@ namespace Laugh.TypeOfShoots
 			{
 				spawnList =  new List<Node2D>();
 				spawmNode = (Node2D) Spawn.Instance();
+				ModifyPosition2d(spawmNode);
 				spawnList.Add(spawmNode);
 				Entity.CallDeferred("add_child", spawmNode);
 			}
-			
 		}
 		
-		public void ModifyPosition2d()
+		
+		public void ModifyPosition2d(Node2D spawn)
 		{
-			var angle = 360 / CountSpawn;
-			foreach (var n in spawnList)
-			{
-				GD.Print(angle.ToString());
-				angle += angle;
-			}
-			//for (var i = 1; i != CountSpawn; i++) spawnList[i].RotationDegrees += angle * i;
+			var position2D = spawn.GetChild<Position2D>(0);
+			position2D.RotationDegrees = sumAngle;
+			sumAngle += angle;
 		}
 		
 
