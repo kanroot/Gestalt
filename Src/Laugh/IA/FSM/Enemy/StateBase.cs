66297a0f4f696c4c448e1275cf287da65168dc36
kan <root.kan.ss@gmai.com>
2021-09-29 16:40:50 +0000

    Separación de la logica de la vida y la vida de los boss

--- Src/Laugh/IA/FSM/Enemy/StateBase.cs
+++ Src/Laugh/IA/FSM/Enemy/StateBase.cs
@@ -1,9 +1,38 @@
+using Godot;
+using Laugh.IA.Enemy;
+using Laugh.TypeOfMovement;
+using Laugh.TypeOfShoots;
+
 namespace Laugh.IA.FSM.Enemy
 {
 	public abstract class StateBase
 	{
+		protected PackedScene Bullet;
+		protected int CountSpawn;
 		protected StateBase CurrentState;
-		public abstract void OnEnter();
+		protected int DegreesToRotate;
+		protected int DirectionToRotate;
+		protected KinematicBody2D Entity;
+		protected PackedScene SpawnBullet;
+		protected float SpeedBullet;
+		protected MovementBase MovementBase;
+		protected ShootBase ShootBase;
+		protected Shoot ShootNode;
+		protected IA.Enemy.Movement MovementNode;
+
+		protected StateBase(Shoot shootNode, IA.Enemy.Movement movementNode, PackedScene spawn, int countSpawn,
+			PackedScene bullet, float speedBullet,
+			KinematicBody2D entity, int directionToRotation, int degreesRotate)
+		{
+			ShootNode = shootNode;
+			MovementNode = movementNode;
+			ShootBase = new ShootCircleEnemy(spawn, countSpawn, bullet, speedBullet, entity, directionToRotation,
+				degreesRotate);
+		}
+
+		public abstract void OnEnter(
+		);
+
 		public abstract void OnExit();
 	}
 }
--- Src/Laugh/Life/LifeBase.cs
+++ Src/Laugh/Life/LifeBase.cs
@@ -4,45 +4,28 @@ namespace Laugh.Life
 {
 	public abstract class LifeBase : Node
 	{
-		private CanvasLayer barCanvas;
-		private TextureProgress barLife;
-		private KinematicBody2D entity;
-		[Export] private NodePath entityPath;
-		[Export] private float maxhealth;
-		[Export] private float health;
-		[Export] private PackedScene lifeBar;
-		[Export] private PackedScene packedSceneRadius;
+		[Export] protected float Health;
+		[Export] protected float MaxHealth;
 		private Area2D area2DCollision;
+		[Export] private NodePath entityPath;
+		[Export] private PackedScene packedSceneRadius;
+		protected KinematicBody2D Entity;
 
 		public override void _Ready()
 		{
-			barCanvas = (CanvasLayer)lifeBar.Instance();
-			barLife = barCanvas.GetChild<TextureProgress>(0);
-			SetBarLife(health, maxhealth);
-			entity = GetNode<KinematicBody2D>(entityPath);
-			entity.CallDeferred("add_child", barCanvas);
-			entity.Connect("ready", this, nameof(AddCollisionShape));
-			entity.Connect("ready", this, nameof(AddConnect));
-		}
-
-		private void SetBarLife(float currentLife)
-		{
-			barLife.Value = currentLife;
-		}
-		private void SetBarLife(float currentLife, float maxLife)
-		{
-			barLife.MaxValue = maxLife;
-			barLife.Value = currentLife;
+			Entity = GetNode<KinematicBody2D>(entityPath);
+			Entity.Connect("ready", this, nameof(AddCollisionShape));
+			Entity.Connect("ready", this, nameof(AddConnect));
 		}
 
 		private void AddCollisionShape()
 		{
 			area2DCollision = (Area2D)packedSceneRadius.Instance();
 			var collisionShape = area2DCollision.GetChild<CollisionShape2D>(0);
-			var entityShape = entity.GetChild<CollisionShape2D>(0);
+			var entityShape = Entity.GetChild<CollisionShape2D>(0);
 			collisionShape.Scale = entityShape.Scale;
 			collisionShape.Shape = entityShape.Shape;
-			entity.AddChild(area2DCollision);
+			Entity.AddChild(area2DCollision);
 		}
 
 		private void AddConnect()
@@ -50,19 +33,14 @@ namespace Laugh.Life
 			area2DCollision.Connect("area_entered", this, nameof(ShootEnter));
 		}
 
-		protected void GetDamage(float damage)
-		{
-			health -= damage;
-			SetBarLife(health);
-		}
+		protected abstract void GetDamage(float damage);
 
 		public abstract void ShootEnter(Area2D bullet);
-		
-		//implementar como señal
+
 		protected void Death()
 		{
-			if (health > 0) return;
-			entity.QueueFree();
+			if (Health > 0) return;
+			Entity.QueueFree();
 		}
 	}
 }
