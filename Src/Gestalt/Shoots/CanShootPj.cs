220371b47a3ff380ef9e8660e1bd97802fe4f9e5
kan <root.kan.ss@gmail.com>
2021-10-13 22:20:43 -0300

    Limpieza general

--- Src/Gestalt/Shoots/CanShootPj.cs
+++ Src/Gestalt/Shoots/CanShootPj.cs
@@ -7,10 +7,10 @@ namespace Gestalt.Shoots
 	{
 		[Export] private NodePath bulletPath;
 		[Export] protected PackedScene BulletScene;
+		[Export] private NodePath rotatePath;
 		private bool mouserOverPlayer;
 		private Node2D rotate;
-		[Export] private NodePath rotatePath;
-
+		
 		public override void _Ready()
 		{
 			base._Ready();
