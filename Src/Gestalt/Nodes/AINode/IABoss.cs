42dbfe56a6423829a54794f499924d00e1c596a4
kan <root.kan.ss@gmail.com>
2021-10-17 22:50:48 -0300

    IA de los boses

---
+++ Src/Gestalt/Nodes/AINode/IABoss.cs
@@ -0,0 +1,74 @@
+using Gestalt.AI.FSM.EnemyStates.Demon;
+using Gestalt.AI.FSM.Resources;
+using Gestalt.Life;
+using Gestalt.Nodes.EnemyNodes;
+using Godot;
+
+namespace Gestalt.Nodes.AINode
+{
+	public class IABoss : AIBase
+	{
+		[Export()] private NodePath lifePath;
+		private LifeBoss lifeBoss;
+		[Export] private DemonState resourceOne;
+		[Export] private DemonState resourceTwo;
+		private StateOne stateOne;
+		private StateTwo stateTwo;
+
+		public override void _Ready()
+		{
+			base._Ready();
+			lifeBoss = GetNode<LifeBoss>(lifePath);
+			BuildStates();
+			lifeBoss.Connect("FirstThird", this, nameof(EnterStateOne));
+			lifeBoss.Connect("SecondThird", this, nameof(EnterStateTwo));
+		}
+		
+
+		protected override void EnterStateOne()
+		{
+			stateOne.OnEnter();
+		}
+
+		protected override void EnterStateTwo()
+		{
+			stateOne.OnExit();
+			stateTwo.OnEnter();
+		}
+
+		protected override void EnterStateThree()
+		{
+			throw new System.NotImplementedException();
+		}
+		
+		protected override void BuildStates()
+		{
+			stateOne = new StateOne(
+				NodeShoot,
+				NodeMovement,
+				Entity,
+				resourceOne.Spawn,
+				resourceOne.Bullet,
+				resourceOne.CountNodes,
+				resourceOne.SpeedBullet,
+				resourceOne.Degrees,
+				resourceOne.Direction,
+				resourceOne.SpeedMovement
+			);
+			
+			stateTwo = new StateTwo(
+				NodeShoot,
+				NodeMovement,
+				Entity,
+				resourceOne.Spawn,
+				resourceOne.Bullet,
+				resourceOne.CountNodes,
+				resourceOne.SpeedBullet,
+				resourceOne.Degrees,
+				resourceOne.Direction,
+				resourceOne.SpeedMovement
+			);
+			
+		}
+	}
+}
