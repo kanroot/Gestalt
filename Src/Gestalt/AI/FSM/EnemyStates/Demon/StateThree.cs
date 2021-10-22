e8a02d5b95cd1eb15b4c3936c52fccac2a7310f0
kan <root.kan.ss@gmail.com>
2021-10-22 20:56:42 -0300

    Limpieza general de los estados del demon

--- Src/Gestalt/AI/FSM/EnemyStates/Demon/StateThree.cs
+++ Src/Gestalt/AI/FSM/EnemyStates/Demon/StateThree.cs
@@ -5,29 +5,16 @@ using Godot;
 
 namespace Gestalt.AI.FSM.EnemyStates.Demon
 {
-	public class StateThree : StateBase
+	public class StateThree : StateBaseDemon
 	{
 		private readonly MovementBounce movementBounce;
 		private readonly ShootCircleEnemy shootCircleEnemy;
-
-		public StateThree(
-			ShootNode shootNode,
-			MovementNode movementNode,
-			KinematicBody2D entity,
-			PackedScene spawn,
-			PackedScene bullet,
-			int countNodes,
-			int speedBullet,
-			int degrees,
-			int direction,
-			int speedMovement
-		) : base(shootNode, movementNode, entity)
-
+		
+		public StateThree(ShootNode shootNode, MovementNode movementNode, KinematicBody2D entity, PackedScene spawn, PackedScene bullet, int countNodes, int speedBullet, int degrees, int direction, int speedMovement) : base(shootNode, movementNode, entity, spawn, bullet, countNodes, speedBullet, degrees, direction, speedMovement)
 		{
 			shootCircleEnemy = new ShootCircleEnemy(spawn, countNodes, bullet, speedBullet, Entity, direction, degrees);
 			movementBounce = new MovementBounce(Entity, speedMovement);
 		}
-
 
 		public override void OnEnter()
 		{
@@ -47,5 +34,7 @@ namespace Gestalt.AI.FSM.EnemyStates.Demon
 			ShootNode.CanShoot = false;
 			shootCircleEnemy.KillNodes();
 		}
+
+		
 	}
 }
