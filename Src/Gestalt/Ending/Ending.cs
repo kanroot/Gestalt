d1f0d28ab01b699578eb673f37b0aa5c0e9e83cc
kan <root.kan.ss@gmail.com>
2021-10-25 17:04:02 -0300

    Nuevo ending añadido, junto al texto de reinciar

--- Src/Gestalt/Ending/Ending.cs
+++ Src/Gestalt/Ending/Ending.cs
@@ -6,39 +6,79 @@ namespace Gestalt.Ending
 {
 	public class Ending : Control
 	{
-		private readonly Random rnd = new Random();
-		private HBoxContainer containerText;
+		private Button buttonMain;
 		private readonly List<string> endingText = new List<string>();
 		private readonly List<Vector2> linesDestiny = new List<Vector2>();
 		private readonly List<Vector2> linesFrom = new List<Vector2>();
+		private readonly Random rnd = new Random();
+		private HBoxContainer containerText;
+		private int count;
+		private TextureRect gestalt;
+		private int height;
 		private Label textEnding;
 		private Vector2 viewport;
 		private int width;
-		private int height;
-		private int count;
-		[Export()]public int DeathCount { get; set; }
+		[Export] public int DeathCount { get; set; }
 
 		public override void _Ready()
 		{
+			GetChild();
+			ScreenSize();
+			ChoseImageOrText();
+			count = 0;
+		}
+
+		private void GetChild()
+		{
 			containerText = GetChild<HBoxContainer>(1);
+			gestalt = GetChild<TextureRect>(2);
+			buttonMain = GetChild<Button>(3);
 			textEnding = containerText.GetChild<Label>(0);
-			ChoseText();
+			AddConnections();
+		}
+
+		private void AddConnections()
+		{
+			buttonMain.Connect("pressed", this, nameof(Textprueba));
+		}
+
+		private void Textprueba()
+		{
+			GD.Print("wuea" +
+			         "");
+		}
+		private void ScreenSize()
+		{
 			viewport = GetViewport().Size;
 			height = (int)viewport.y;
 			width = (int)viewport.x;
-			count = 0;
 		}
 		
+		private void ChoseImageOrText()
+		{
+			if (DeathCount < 100)
+			{
+				SetText();
+				var selector = rnd.Next(0, endingText.Count);
+				textEnding.Text = endingText[selector];
+				containerText.Alignment = BoxContainer.AlignMode.Center;
+			}
+			else
+			{
+				gestalt.Visible = true;
+			}
+		}
+
 		public override void _Process(float delta)
 		{
 			if (count > DeathCount) return;
-			count += 1;
 			CreateLines();
-
 		}
+
 
 		private void CreateLines()
 		{
+			count += 1;
 			var from = new Vector2(0, rnd.Next(0, height));
 			var destiny = new Vector2(width, rnd.Next(0, height));
 			linesFrom.Add(from);
@@ -50,15 +90,7 @@ namespace Gestalt.Ending
 		{
 			for (var i = 0; i < linesFrom.Count; i++) DrawLine(linesFrom[i], linesDestiny[i], new Color(1, 1, 1));
 		}
-
-
-		private void ChoseText()
-		{
-			SetText();
-			var selector = rnd.Next(0, endingText.Count);
-			textEnding.Text = endingText[selector];
-		}
-
+		
 		private void SetText()
 		{
 			endingText.Add("END GAME ?");
