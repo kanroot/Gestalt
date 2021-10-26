22276c0d4844ebd2d4d7e0edfb13b530dc4558bc
kan <root.kan.ss@gmail.com>
2021-10-25 23:51:26 -0300

    añadido el estatico de skull

--- Src/Gestalt/Text/CanMovementText.cs
+++ Src/Gestalt/Text/CanMovementText.cs
@@ -7,17 +7,29 @@ namespace Gestalt.Text
 	{
 		[Export] private int degreesRotate;
 		[Export] private NodePath pathTextRotated;
+		[Export()] private NodePath bodyPath;
 		private Sprite textRotated;
+		private Sprite body;
+		private int count;
+		private int secondCount;
 
 		public override void _Ready()
 		{
 			textRotated = GetNode<Sprite>(pathTextRotated);
+			body = GetNode<Sprite>(bodyPath);
 		}
 
-		public override void _PhysicsProcess(float delta)
+		public override void _Process(float delta)
 		{
+			count += degreesRotate; 
 			var degreesToRadiant = Math.PI / 180 * degreesRotate;
 			textRotated.Rotate((float)degreesToRadiant);
+			if (count < 360) return;
+			body.Rotate((float)degreesToRadiant);
+			secondCount += 1;
+			if (secondCount < 360) return;
+			count = 0;
+			secondCount = 0;
 		}
 	}
 }
