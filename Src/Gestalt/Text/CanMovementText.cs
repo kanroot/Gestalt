cb8f22031da5b65a0bd75c3b2d36a94c9da5f42e
kan <root.kan.ss@gmail.com>
2021-10-27 16:14:18 -0300

    Traspaso de metodos propios de los static hacia otra clase

--- Src/Gestalt/Text/CanMovementText.cs
+++ Src/Gestalt/Text/CanMovementText.cs
@@ -5,42 +5,20 @@ namespace Gestalt.Text
 {
 	public class CanMovementText : Node
 	{
-		[Export] private int degreesRotate;
+		[Export] public int DegreesRotate;
+		protected float DegreesToRadiant;
 		[Export] private NodePath pathTextRotated;
-		[Export()] private NodePath bodyPath;
-		[Export()] private Boolean CanMoveBody { get; set; }
 		private Sprite textRotated;
-		private Sprite body;
-		private int count;
-		private int secondCount;
-		private double degreesToRadiant;
 
 		public override void _Ready()
 		{
 			textRotated = GetNode<Sprite>(pathTextRotated);
-			body = GetNode<Sprite>(bodyPath);
 		}
 
 		public override void _Process(float delta)
 		{
-			count += degreesRotate; 
-			degreesToRadiant = Math.PI / 180 * degreesRotate;
-			textRotated.Rotate((float)degreesToRadiant);
-			RotateBody(count);
-			
-		}
-
-		private void RotateBody(int degrees)
-		{
-			if (degrees > 180 && CanMoveBody){
-				body.Rotate((float)degreesToRadiant);
-				secondCount++;
-			}
-			if (secondCount >= 360)
-			{
-				count = 0;
-				secondCount = 0;
-			};
+			DegreesToRadiant = (float)(Math.PI / 180 * DegreesRotate);
+			textRotated.Rotate(DegreesToRadiant);
 		}
 	}
 }
